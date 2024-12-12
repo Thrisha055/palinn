d5191ce806dccb8ee28186688cd49fbde0cd8b48
Thrisha055 <niceambala@gmail.com>
2024-12-12 16:13:49 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,13 +1,20 @@
-public class Palindrome{
+public class Palindrome {
     public static void main(String[] args) {
-        System.out.print("Enter a string: ");
-        String str = nextLine();
-        String reversedStr="";
+        // Hardcoded input string
+        String str = "madam";  // Change this value to test other strings
         
+        // Convert string to lower case to make it case-insensitive
+        str = str.toLowerCase();
+        
+        // Variable to store the reversed string
+        String reversedStr = "";
+        
+        // Loop through the string from the end to the beginning
         for (int i = str.length() - 1; i >= 0; i--) {
             reversedStr += str.charAt(i);
         }
         
+        // Check if the original string is equal to its reversed version
         if (str.equals(reversedStr)) {
             System.out.println(str + " is a palindrome.");
         } else {
